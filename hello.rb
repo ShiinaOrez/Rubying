4bfcd352690efdf06af13d413146ee0d73a96f34
ShiinaOrez@Dev <shiinaorez@gmail.com>
2019-05-08 22:53:37 +0800

    由 Cloud Studio 更新

--- hello.rb
+++ hello.rb
@@ -1,8 +1,33 @@
 #!/usr/bin/ruby -w
 
-def hello
-    out = "hello, ruby";
-    puts out
-    end
+=begin
+This is Shiina's first ruby problem.
+=end
 
+END {
+  puts "Ruby end!"
+}
+
+def hello
+  out = "hello, ruby";
+  puts out
+end
 hello
+
+=begin
+puts <<EOF
+"I want learn ruby soon."
+"And..."
+EOF
+=end
+
+def add
+  a = 1
+  b = 2.0
+  puts a + b  
+end
+add
+
+BEGIN {
+  puts "Ruby begin!"
+}
